67af6928e73db42f764449e46224b699d841cce5
Zeliha Nur Uztimür <78546860+uztnur@users.noreply.github.com>
2021-07-01 19:35:42 +0300

    JAVA ÖĞRENİMİ VE UYGULAMALARI
    JAVA ÖĞRENİMİ VE UYGULAMALARI

---
+++ ders11.java
@@ -0,0 +1,27 @@
+package baslangic1;
+
+public class ders11 {
+
+	public static void main(String[] args) {
+		// if,else ve else if koşulların sonuçlarını kontrol etmemize yarar.
+		// Sadece 2 sonuç ihtimali varsa if ve else kullanırız fakat 2'den fazla sonuç ihtimali varsa else if de  kullanırız.
+			int a = 5;
+			int b = 12;
+			int c = 20;
+			if (a == b) { System.out.println("a = b 'dir.");
+			} else {
+			  System.out.println(" a, b'ye eşit değildir."); 
+			  }//Burada 2 sayının eşit olup olmadığını kontrol ettik.Sadece 2 sonuç ihtimali olduğundan if ve else kullandık.
+			 
+			if ((a < b) && (a < c)) {
+				System.out.println("En küçük sayı a ' dır.");
+			} else if ((c < b) && (c < a)) {
+				System.out.println("En küçük sayı c ' dir.");
+			} else {
+				System.out.println("En küçük sayı b ' dir.");
+			} //Burada ise 3 sayıdan en küçük olanı bulduk.Burada 3 sonuç ihtimali olduğundan else if de kullandık.
+		
+
+	}
+
+}
